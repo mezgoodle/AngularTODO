d334db6220980b8ee74fc71ab76e709817ea8a5f
Maksym Zavalniuk <mezgoodle@gmail.com>
2023-02-24 16:36:29 +0200

    Make some changes

---
+++ frontend/src/app/components/button/button.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  templateUrl: './button.component.html',
+  styleUrls: ['./button.component.css'],
+})
+export class ButtonComponent {
+  @Input() text: string | undefined;
+  @Input() color: string | undefined;
+
+  onClick() {
+    console.log('click');
+  }
+}
