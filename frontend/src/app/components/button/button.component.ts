ba0e319f02e44b3d96e0ff4f3d92de5e9cde8847
Maksym Zavalniuk <mezgoodle@gmail.com>
2023-02-25 10:09:45 +0200

    Emit events in buttons

--- frontend/src/app/components/button/button.component.ts
+++ frontend/src/app/components/button/button.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-button',
@@ -8,8 +8,9 @@ import { Component, Input } from '@angular/core';
 export class ButtonComponent {
   @Input() text: string | undefined;
   @Input() color: string | undefined;
+  @Output() btnClick = new EventEmitter();
 
   onClick() {
-    console.log('click');
+    this.btnClick.emit();
   }
 }
--- frontend/src/app/components/header/header.component.ts
+++ frontend/src/app/components/header/header.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
   title: string = 'ToDo App';
+
+  toggleAddTask() {
+    console.log('toggle');
+  }
 }
