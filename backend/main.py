a89cc02dc4d7225edf010fd1bdcf58ab57597bf3
Maksym Zavalniuk <mezgoodle@gmail.com>
2023-03-01 16:44:36 +0200

    Add sqlmodel

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,46 @@
+from typing import Optional
+
 from fastapi import FastAPI
+from sqlmodel import Field, Session, SQLModel, create_engine, select
+
+
+class Task(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    text: str = Field(index=True)
+    day: str
+    reminder: bool
+
+
+sqlite_file_name = "database.db"
+sqlite_url = f"sqlite:///{sqlite_file_name}"
+
+connect_args = {"check_same_thread": False}
+engine = create_engine(sqlite_url, echo=True, connect_args=connect_args)
+
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
 
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.on_event("startup")
+def on_startup():
+    create_db_and_tables()
+
+
+@app.post("/tasks/")
+def create_task(task: Task):
+    with Session(engine) as session:
+        session.add(task)
+        session.commit()
+        session.refresh(task)
+        return task
+
+
+@app.get("/tasks/")
+def read_tasks():
+    with Session(engine) as session:
+        tasks = session.exec(select(Task)).all()
+        return tasks
